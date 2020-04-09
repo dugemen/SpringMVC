7cf471c155a094d4c697b16124a7f20d646d8d2e
dugemen <1071538120@qq.com>
2020-04-09 15:22:38 +0800

    修改过后的实体类

--- src/com/zhongruan/bean/Teacher.java
+++ src/com/zhongruan/bean/Teacher.java
@@ -1,4 +1,38 @@
 package com.zhongruan.bean;
 
 public class Teacher {
+    private int stuid;
+    private String stuName;
+
+    public Teacher(int stuid, String stuName) {
+        this.stuid = stuid;
+        this.stuName = stuName;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "stuid=" + stuid +
+                ", stuName='" + stuName + '\'' +
+                '}';
+    }
+
+    public Teacher() {
+    }
+
+    public int getStuid() {
+        return stuid;
+    }
+
+    public void setStuid(int stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
 }
